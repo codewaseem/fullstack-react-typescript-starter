1268755bd7568dec64e5180b41c0a5554b1b5d9c
=Waseem Ahmed <codewaseem@gmail.com>
2018-04-09 00:00:00 +0530

    change to heroku url

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
 export const clientAxios = Axios.create({
-  // baseURL : "https://infinite-mesa-64934.herokuapp.com"
-  baseURL : "http://localhost:3000"  
+  baseURL : "https://infinite-mesa-64934.herokuapp.com"
+  // baseURL : "http://localhost:3000"  
 });
