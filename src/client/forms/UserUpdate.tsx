d19d2ce379725ef2ae0ba810bda9943ad24c737d
=Waseem Ahmed <codewaseem@gmail.com>
2018-04-13 11:04:47 +0530

    fix reference

--- src/client/forms/UserUpdate.tsx
+++ src/client/forms/UserUpdate.tsx
@@ -3,7 +3,7 @@ import { Field, reduxForm } from "redux-form";
 import { SubmitButton } from "../components";
 import { generateReduxFormComponent } from "./formUtils";
 import { UserFields } from "../../schemaConfigs/userSchema";
-const copy = {...UserFields};
+const copy = { ...UserFields };
 delete copy.password;
 
-export default generateReduxFormComponent("UserUpdateForm", UserFields);
+export default generateReduxFormComponent("UserUpdateForm", copy);
